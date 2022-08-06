c6832391a0984186e0c4381245d8068c9dc4b5c4
Y-wo <y-wo@gmx.de>
2022-08-06 12:03:50 +0200

    save

--- script.js
+++ script.js
@@ -1,25 +1,102 @@
 'use strict'
 
-let subject1 = "WPF"
-let teacher1 = "Herr Beicht"
+/*
+TODO:
+    - input links, output rechts
+    - gesamtes Ausspucken (unten - automatisch immer)
+    - button und Funktion, Sätze zu ändern
+    - Sätze verbessern
+    - Datumsfunktion
+
+ */
+
+
+let subjects = [
+    {
+        title : 'WPF',
+        teacher : 'Herr Beicht',
+    },
+    {
+        title : 'Deutsch',
+        teacher : 'Frau Caglar',
+    },
+    {
+        title : 'Englisch',
+        teacher : 'Frau Caglar',
+    },
+    {
+        title : 'Lernfeld 1',
+        teacher : 'Frau Führich-Albert',
+    },
+    {
+        title : 'Lernfeld 2',
+        teacher : 'Herr Schäfer',
+    },
+    {
+        title : 'Lernfeld 2',
+        teacher : 'Frau Gau',
+    },
+    {
+        title : 'Lernfeld 3',
+        teacher : 'Herr Heyeckhaus',
+    },
+    {
+        title : 'Lernfeld 4',
+        teacher : 'Herr Decker',
+    },
+    {
+        title : 'Lernfeld 5',
+        teacher : 'Herr Donnarumma',
+    },
+    {
+        title : 'SoWi',
+        teacher : 'Frau Ruf',
+    }
+]
+
 
 
 
 document.addEventListener("DOMContentLoaded", function () {
 
-    $('#testButton').click(function() {
-        console.log("test123")
-        $('#testOutput').append("test123")
-    })
+    let counter = 1
+    for(let subject of subjects){
 
-    $(".inputSubject1Js").val(subject1)
-    $(".inputTeacher1Js").val(teacher1)
+        console.log(counter)
+        //div
+        let thisDiv = document.createElement("div")
+        thisDiv.className = "thisDiv col-10 d-flex flex-column align-items-center mb-3"
 
-    // $(".output1Js").val(function (subject1, teacher1 ){
-    //
-    // })
+        //heading
+        let heading = document.createElement("h5")
+        heading.append(subject.title + " - " + subject.teacher)
+        //topic input
+        let topicInput = document.createElement("input")
+        topicInput.placeholder = 'Fach' + counter
+        topicInput.className = 'topic' + counter
 
-    $(".output1Js").append( writeSentence(subject1, teacher1))
+        //output
+        let output = document.createElement("div")
+        output.className = 'output' + counter + ' align-self-start'
+
+        thisDiv.append(heading)
+        thisDiv.append(topicInput)
+        thisDiv.append(output)
+        $(".mainDivJs").append(thisDiv)
+
+        //events
+        topicInput.addEventListener("keyup", function(event){
+            if(event.key === "Enter"){
+                subject.topic = topicInput.value
+                console.log(subject.topic)
+                output.append(writeSentence(subject.title, subject.teacher, subject.topic))
+            }
+        })
+
+        counter++
+
+    }
+
 
 
 
@@ -27,6 +104,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-function writeSentence(subject1, teacher1){
-    return "hallo " + subject1 + " " + teacher1
+// let sentence1 = `In ${subject} bei ${teacher} haben wir uns mit dem Thema blabla beschäftigt`
+
+function writeSentence(subject, teacher, topic){
+    let sentenceArray = [
+        'In Fach ' + subject + ' bei ' + ' haben wir uns mit dem Thema ' + topic + ' beschäftigt.',
+        teacher + ' behandelte in Fach ' + subject + ' besonders das Thema ' + topic,
+    ]
+
+    let randomNumber = Math.floor(Math.random() * sentenceArray.length)
+    return sentenceArray[randomNumber]
 }
